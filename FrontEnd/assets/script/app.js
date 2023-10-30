4d2396c27fe04234fbedaaeb3137522d281a6a7c
Maquet Dorian <takenzmr@gmail.com>
2023-10-30 11:51:54 +0100

    Update

--- FrontEnd/assets/script/app.js
+++ FrontEnd/assets/script/app.js
@@ -1,46 +1,42 @@
-let reponse = await fetch('/api/works');
-let works = await reponse.json();
-console.log(works);
+// Récupération des images et du texte pour "Mes Projets"
 
-async function updateWorks() {
-    reponse = await fetch('/api/works');
-    works = await reponse.json();
-}
+let allProjects = [];
 
-const catalogue = document.querySelector('#portfolio');
-const header = document.querySelector('header');
-const main = document.getElementById('main');
-const introduction = document.querySelector('#introduction');
-const contact = document.querySelector('#contact');
+fetch('http://localhost:5678/api/works')
+    .then (response => response.json())
+    .then(data => {
+        allProjects = data;
+        displayProjects(data);
+});
+    
 
-// Création du HTML du catalogue
-function createPortfolio() {
-    const worksTitleDiv = document.createElement('div');
-    worksTitleDiv.className = 'worksTitleDiv';
-    const worksTitle = document.createElement('h2');
-    worksTitle.innerText = 'Mes Projets';
-  
-    const filters = document.createElement('div');
-    filters.className = 'filters';
-  
-    const gallery = document.createElement('div');
-    gallery.className = 'gallery';
-  
-    catalogue.style.display = null;
-  
-    catalogue.appendChild(worksTitleDiv);
-    worksTitleDiv.appendChild(worksTitle);
-    catalogue.appendChild(filters);
-    catalogue.appendChild(gallery);
-    const sectionWorks = document.querySelector('.gallery');
-    const divFilters = document.querySelector('.filters');
-  
-    const editWorksBtn = document.createElement('p');
-    editWorksBtn.innerHTML = `<a href="#modale" class="openModale">
-          <i class="fa fa-light fa-pen-to-square"></i> 
-          modifier </a>`;
-    editWorksBtn.setAttribute('id', 'editWorksBtn');
-    editWorksBtn.className = 'editWorksBtn';
-    editWorksBtn.style.display = 'none';
-  
-    worksTitleDiv.appendChild(editWorksBtn);}
+
+function displayProjects(projects) {
+    const createGallery = () => {
+    const gallery = /*html*/`
+        <div class="gallery">
+    ${projects.map((project) => /*html*/ `
+        <figure>
+            <img src="${project.imageUrl}" alt="${project.title}" />
+            <figcaption>${project.title}</figcaption> 
+            </figure>
+        `).join("")}
+    </div>
+`;
+    
+    
+// Cherche la section avec l'id "portfolio"
+    
+const portfolioSection = document.getElementById('portfolio');
+    
+// Vérifie s'il y a déja une galerie, si oui, la remplace const existingGallery = document queryselector ("•gallery');
+const existingGallery = document.querySelector('.gallery');
+if (existingGallery) {
+existingGallery.outerHTML = gallery;
+} else {
+// Sinon, ajoute la nouvelle galerie à l'intérieur de la section "portfolio"
+    portfolioSection.insertAdjacentHTML('beforeend', gallery);
+}};
+    
+createGallery(); // Appel de la fonction createdallery pour créer la galerie d'images
+};

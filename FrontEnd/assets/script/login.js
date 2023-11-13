350ae2a4c318e269b5a90cdcad8276fec25b8b2b
Maquet Dorian <takenzmr@gmail.com>
2023-11-13 15:13:19 +0100

    Update

---
+++ FrontEnd/assets/script/login.js
@@ -0,0 +1,6 @@
+// Création de la fonction de connexion
+
+async function sendId() {
+    const formId = document.querySelector(".login_form");
+    const errorMessage = document.querySelector(".error-message");
+}

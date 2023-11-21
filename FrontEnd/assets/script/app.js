376366ab0cd377a7987c0572412754a06f5afc31
Maquet Dorian <takenzmr@gmail.com>
2023-11-21 14:48:02 +0100

    Update

--- FrontEnd/assets/script/app.js
+++ FrontEnd/assets/script/app.js
@@ -8,78 +8,63 @@ fetch(apiWorks)
     renderProject(works)
     renderProjectModal(works)
     allProject = works
-  });
+});
   fetch(apiCategories)
       .then(response => response.json())
       .then(categories => {
         renderCategories(categories);
-      });
+});
+
+
 
 function renderProject (works) {  
-    let project = document.querySelector(".gallery");
+  let project = document.querySelector(".gallery");
     
-    project.innerHTML = "";
+  project.innerHTML = "";
 
+  for (let i = 0; i < works.length; i++){
+    let figure = document.createElement("figure");
+    let img = document.createElement("img");
+    let figcaption = document.createElement("figcaption");
+
+    img.setAttribute("src", works[i].imageUrl);
+    figcaption.setAttribute("alt", works[i].title);
+    img.setAttribute("crossorigin", "anonymous");
+    figcaption.innerHTML = works[i].title;
+    project.appendChild(figure);
+    figure.append(img, figcaption);
+}}
     
 
-    for (let i = 0; i < works.length; i++){
-        let figure = document.createElement("figure");
-        let img = document.createElement("img");
-        let figcaption = document.createElement("figcaption");
-
-        img.setAttribute("src", works[i].imageUrl);
-        figcaption.setAttribute("alt", works[i].title);
-        img.setAttribute("crossorigin", "anonymous");
-
-        figcaption.innerHTML = works[i].title;
-
-        
-        project.appendChild(figure);
-        figure.append(img, figcaption);
-    }
-      
-    }
-
-    function renderCategories (categories) {  
+function renderCategories (categories) {  
     
-      let filtre = document.querySelector(".filtre");
-  
-      filtre.innerHTML = "";
+  let filtre = document.querySelector(".filtre");
+  filtre.innerHTML = "";
 
       
-let allButton = document.createElement("button");  
-      allButton.innerHTML = "Tous";
-      filtre.appendChild(allButton);
-      allButton.addEventListener("click", function () {
-        renderProject(allProject);
-        
-      });
+  let allButton = document.createElement("button");  
+    allButton.innerHTML = "Tous";
+    filtre.appendChild(allButton);
+    allButton.addEventListener("click", function () {
+    renderProject(allProject);
+  });
     
-      for (let i = 0 ; i < categories.length; i++){
+  for (let i = 0 ; i < categories.length; i++){
 
-      
+    let button = document.createElement("button");
+      button.setAttribute("value", categories[i].name);
+      button.innerHTML = categories[i].name;
+      filtre.appendChild(button);
+      button.addEventListener("click", function (){
+    const filterProject = allProject.filter(function(project){
+      console.log(categories[i].id, project.categoryId)
+      return project.categoryId === categories[i].id
+    });
+    renderProject(filterProject)
+})}}
 
 
-        let button = document.createElement("button");
-        button.setAttribute("value", categories[i].name);
-        button.innerHTML = categories[i].name;
-        filtre.appendChild(button);
-        button.addEventListener("click", function (){
-        const filterProject = allProject.filter(function(project){
-                
-                console.log(categories[i].id, project.categoryId)
-
-                return project.categoryId === categories[i].id
-              });
-            
-              renderProject(filterProject)
-            
-
-        }) 
-        
-}
-}
-// REDIRECTION VERS LA PAGE ADMIN
+// Redirection vers la page d'admin
 
 const loginBtn = document.getElementById('loginBtn');
 const logoutBtn = document.getElementById('logoutBtn');
@@ -118,7 +103,7 @@ if (localStorage.getItem("jeton")) {
   modeEdition.style.display= "none"
 }
 
-// DECONNEXION 
+// Déconnexion
 logoutBtn.addEventListener('click', function() {
 
   // Supprimer le token du local storage
@@ -130,3 +115,92 @@ logoutBtn.addEventListener('click', function() {
   // Afficher le lien de connexion
   loginBtn.style.display = 'inline-block';
 });
+
+
+//  Création Modal
+
+const openModal = function(e) {
+  e.preventDefault();
+  const target = document.querySelector(e.target.getAttribute("href"));
+  target.style.display = "block";
+  target.removeAttribute("aria-hidden");
+  target.setAttribute("aria-modal", "true");
+};
+
+const closeModal = function(e) {
+  e.preventDefault();
+  const modal = e.target.closest(".modal");
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+  modal.removeAttribute("aria-modal");
+};
+
+document.querySelectorAll(".js-modal").forEach(a => {
+  a.addEventListener("click", openModal);
+});
+
+document.querySelectorAll(".js-modal-close").forEach(button => {
+  button.addEventListener("click", closeModal);
+});
+
+window.addEventListener("click", e => {
+  if (e.target.classList.contains("modal")) {
+    e.target.style.display = "none";
+    e.target.setAttribute("aria-hidden", "true");
+    e.target.removeAttribute("aria-modal");
+  }
+});
+
+window.addEventListener("keydown", e => {
+  if (e.key === "Escape" || e.key === "Esc") {
+    document.querySelectorAll(".modal").forEach(modal => {
+      modal.style.display = "none";
+      modal.setAttribute("aria-hidden", "true");
+      modal.removeAttribute("aria-modal");
+    });
+  }
+});
+
+
+// Contenu de la modal
+
+function renderProjectModal(works) {
+  const modalProject = document.querySelector(".project-modal");
+  modalProject.innerHTML = "";
+  const tokenJeton = window.localStorage.getItem("jeton")
+  const projectModal = document.querySelector('.project-modal');
+
+  for (let i = 0; i < works.length; i++) {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    const trashIcon = document.createElement('i');
+    const editModal = document.createElement("p")
+
+
+    img.setAttribute("src", works[i].imageUrl);
+    figcaption.setAttribute("alt", works[i].title);
+    img.setAttribute("crossorigin", "anonymous");
+    trashIcon.classList.add('fa-solid', 'fa-trash-can');
+   
+
+    trashIcon.addEventListener('click', function() {
+      const projectId = works[i].id;
+      fetch(apiWorks + '/' + projectId, {
+        method: 'DELETE',
+        headers: {
+          Authorization : 'Bearer ' + tokenJeton,
+          'Content-Type': 'application/json'
+        }
+      })
+
+      .then(response => response.json())
+      .then(data => {
+        figure.remove();
+      })
+    });
+
+    projectModal.appendChild(figure);
+    figure.append(img, figcaption, trashIcon, editModal,);
+  }
+}
